26d3da5f5db6a60d2d7f7e9646ce8f713fdf9435
Zeinos <53605726+Zeinos@users.noreply.github.com>
2019-08-02 14:16:32 +0400

    Delete index.js

--- .gitignore/index.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-const fs = require("fs");
-
-client.login("NjA2NzI3OTQ2MDg1OTkwNDAx.XUPRhA.mg9PeCrtN1aw7HG18ubMFV3PHC0");
